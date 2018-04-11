524f1caffc392d505c843cf031a5ff03426c27db
Jorge Guerrero <jguerreropedev@gmail.com>
2018-04-11 23:41:59 +0200

    - created splash screen

---
+++ app/src/main/java/com/jguerrerope/tvchallenge/ui/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.jguerrerope.tvchallenge.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.jguerrerope.tvchallenge.R
+import org.jetbrains.anko.startActivity
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * The {@link SplashActivity} of the app that make as point entry
+ */
+class SplashActivity : AppCompatActivity() {
+    companion object {
+        private val SPLASH_DELAY = TimeUnit.SECONDS.toMillis(2)
+    }
+
+    private var delayHandler = Handler()
+
+    /**
+     * Runnable used to route app.
+     */
+    private val runnable: Runnable = Runnable {
+        if (!isFinishing) {
+            startActivity<MainActivity>()
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //execute router runnable with delay
+        delayHandler.postDelayed(runnable, SPLASH_DELAY)
+    }
+
+    public override fun onDestroy() {
+        // remove Runnable router
+        delayHandler.removeCallbacks(runnable)
+        super.onDestroy()
+    }
+}
